a8b76255df5f18635719b300d51cb6aabe4ecf9d
Gera <grsb99@gmail.com>
2021-02-16 17:28:28 +0100

    first commit

---
+++ _nuxt/static/1613492827/averageCalculator/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/averageCalculator", {data:[{}],fetch:[],mutations:void 0});
---
+++ _nuxt/static/1613492827/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002FaverageCalculator","\u002Fcontact","\u002Fevents","\u002Fgenerator","\u002Fgt","\u002Finternship","\u002Fservices"]})
